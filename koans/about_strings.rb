af0606333cf1f82f00f1bfc25aeae545e9d117c0
ralphreid <beresfordjunior@me.com>
2014-07-19 18:42:27 +0100

    working on about_strings koan

--- koans/about_strings.rb
+++ koans/about_strings.rb
@@ -3,12 +3,12 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutStrings < Neo::Koan
   def test_double_quoted_strings_are_strings
     string = "Hello, World"
-    assert_equal __, string.is_a?(String)
+    assert_equal true, string.is_a?(String)
   end
 
   def test_single_quoted_strings_are_also_strings
     string = 'Goodbye, World'
-    assert_equal __, string.is_a?(String)
+    assert_equal true, string.is_a?(String)
   end
 
   def test_use_single_quotes_to_create_string_with_double_quotes
