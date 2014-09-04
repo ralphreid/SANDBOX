cf0306a2824a326da0489b949e4c4f184439f4ab
ralphreid <beresfordjunior@me.com>
2014-09-04 05:22:47 +0100

    about_strings koan

--- koans/about_strings.rb
+++ koans/about_strings.rb
@@ -18,13 +18,13 @@ class AboutStrings < Neo::Koan
 
   def test_use_double_quotes_to_create_strings_with_single_quotes
     string = "Don't"
-    assert_equal __, string
+    assert_equal "Don't", string
   end
 
   def test_use_backslash_for_those_hard_cases
     a = "He said, \"Don't\""
     b = 'He said, "Don\'t"'
-    assert_equal __, a == b
+    assert_equal true, a == b
   end
 
   def test_use_flexible_quoting_to_handle_really_hard_cases
