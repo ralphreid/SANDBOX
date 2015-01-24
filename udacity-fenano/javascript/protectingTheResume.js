450dc25fd20c3cd0c112d30e0c8624e85de4b1a2
Ralph <beresfordjunior@me.com>
2015-01-24 15:40:08 +0000

    initial commit

---
+++ udacity-fenano/javascript/protectingTheResume.js
@@ -0,0 +1,14 @@
+var html =
+	'<script src="http://hackyourwebsite.com/eviljavascript.js"></script>';
+
+var charEscape = function(_html) {
+	var newHTML = _html;
+	// How will you make sure that newHTML doesn't contain any < or > ?
+	// Your code goes here!
+
+	// Don't delete this line!
+	return newHTML;
+};
+
+// Did your code work? The line below will tell you!
+console.log(charEscape(html));
