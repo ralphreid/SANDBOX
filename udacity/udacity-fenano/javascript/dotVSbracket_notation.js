b2cb98f01709a444387b153548c717d5e0207a7b
Ralph <beresfordjunior@me.com>
2015-04-03 12:41:54 +0100

    added full-stack & restructured directory

---
+++ udacity/udacity-fenano/javascript/dotVSbracket_notation.js
@@ -0,0 +1,57 @@
+var weirdObject = {
+  "property": "Time for an astronomy lesson!",
+  "property1": "Cameron's minor in college was astronomy",
+  "property-2": "The 4 Galilean largest moons of Jupiter are:",
+  "property 3": "Io, Ganymede, Callisto, Europa",
+  "property$": "Saturn's moon Enceladus has liquid water ocean under its icy surface",
+  " property": "The Sun contains 99.87% of the mass of the entire solar system",
+  "property()": "There are 5 dwarf planets in our solar system:",
+  "property[]": "Pluto, Ceres, Eris, Haumea, Makemake",
+  "8property": "Mars has two tiny moons: Phobos and Deimos"
+};
+
+// Use console.log() to figure out if dot and/or bracket notation
+// will work to access the properties below. Mark true if you can use dot/bracket
+// notation to access the property, otherwise mark false.
+
+// For example, uncomment the line below to see if you can use dot notation to access `property1`.
+// console.log(weirdObject.property1);
+
+// I'll give you the first answer. The rest are set to false. Try out each property and
+// if you can use dot or bracket notation to access it, change the answer to true!
+
+// property
+var dotNotation0 = true;
+var bracketNotation0 = true;
+
+// property1
+var dotNotation1 = false;
+var bracketNotation1 = false;
+
+// property-2
+var dotNotation2 = false;
+var bracketNotation2 = false;
+
+// property 3
+var dotNotation3 = false;
+var bracketNotation3 = false;
+
+// property$
+var dotNotation4 = false;
+var bracketNotation4 = false;
+
+// *space*property
+var dotNotation5 = false;
+var bracketNotation5 = false;
+
+// property()
+var dotNotation6 = false;
+var bracketNotation6 = false;
+
+// property[]
+var dotNotation7 = false;
+var bracketNotation7 = false;
+
+// 8property
+var dotNotation8 = false;
+var bracketNotation8 = false;
