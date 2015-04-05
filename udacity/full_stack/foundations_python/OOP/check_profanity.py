e3f16caf402bedd77645fddc0a32dffb58c422f7
Ralph <beresfordjunior@me.com>
2015-04-05 15:57:08 +0100

    profanity checker working using external url checker

--- udacity/full_stack/foundations_python/OOP/check_profanity.py
+++ udacity/full_stack/foundations_python/OOP/check_profanity.py
@@ -1,3 +1,5 @@
+import urllib
+
 __author__ = 'ralph'
 
 
@@ -6,5 +8,14 @@ def read_text():
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
+    check_profanity(contents_of_file)
+
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
+
 
 read_text()
