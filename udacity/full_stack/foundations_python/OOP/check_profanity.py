e28537b6bda0b5114f8091f1f45c98a73b7251ca
Ralph <beresfordjunior@me.com>
2015-04-05 16:01:47 +0100

    add logic to print a proper profanity alert

--- udacity/full_stack/foundations_python/OOP/check_profanity.py
+++ udacity/full_stack/foundations_python/OOP/check_profanity.py
@@ -14,8 +14,14 @@ def read_text():
 def check_profanity(text_to_check):
     connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
     output = connection.read()
-    print(output)
+    # print(output)
     connection.close()
+    if "true" in output:
+        print("Profanity Alert!!")
+    elif "false" in output:
+        print("This document has no curse words!")
+    else:
+        print("Could not scan the document properly.")
 
 
 read_text()
