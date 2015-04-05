4be93f97d3c6f0c9a46f8155a76dad670027c9f6
Ralph <beresfordjunior@me.com>
2015-04-05 17:00:03 +0100

    started working on movie website app

---
+++ udacity/full_stack/foundations_python/OOP/movies/entertainment_center.py
@@ -0,0 +1,9 @@
+__author__ = 'ralph'
+
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://en.wikipedia.org/wiki/File:Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=_KFpws6qmuw")
+print(toy_story.storyline)
---
+++ udacity/full_stack/foundations_python/OOP/movies/media.py
@@ -0,0 +1,9 @@
+__author__ = 'ralph'
+
+
+class Movie():
+    def __init__(self, movie_title, movie_story_line, movie_poster_image_url, movie_trailer_youtube_url):
+        self.title = movie_title
+        self.storyline = movie_story_line
+        self.poster_image_url = movie_poster_image_url
+        self.trailer_youtube_url = movie_trailer_youtube_url
