9c1c95613d966527a321a017d415c2d1c0297d70
Ralph <beresfordjunior@me.com>
2015-04-05 17:43:37 +0100

    added avatar movie object

--- udacity/full_stack/foundations_python/OOP/movies/entertainment_center.py
+++ udacity/full_stack/foundations_python/OOP/movies/entertainment_center.py
@@ -6,4 +6,12 @@ toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "http://en.wikipedia.org/wiki/File:Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=_KFpws6qmuw")
+
+avatar = media.Movie(
+    "Avatar",
+    "A marine on an alien planet",
+    "http://en.wikipedia.org/wiki/Avatar_(2009_film)",
+    "http://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg"
+)
 print(toy_story.storyline)
+print(avatar.storyline)
