8a012f4a4d4dfc564adf35b3f2ea0239e64ad015
Ralph <beresfordjunior@me.com>
2015-04-05 13:24:28 +0100

    one turtle makes a square and other makes a circle plus refactored with a draw_square function

--- udacity/full_stack/foundations_python/OOP/mindstorms.py
+++ udacity/full_stack/foundations_python/OOP/mindstorms.py
@@ -2,23 +2,25 @@ __author__ = 'ralph'
 
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
-
+    # Create the turtle brad - Draws a square
     brad = turtle.Turtle()
     brad.shape("triangle")
     brad.color("black")
     brad.speed("fast")
-
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+    draw_square(brad)
+    # Create the turtle Angie - Draws a circle
+    angie = turtle.Turtle()
+    angie.color("blue")
+    angie.shape("arrow")
+    angie.circle(100)
 
     window.exitonclick()
 
