0938aab3958deea0005fc13717e3e6457456ab09
Ralph <beresfordjunior@me.com>
2015-04-05 10:55:37 +0100

    turtle is moving mon - now starting on changing configs and behaviours

---
+++ udacity/full_stack/foundations_python/OOP/mindstorms.py
@@ -0,0 +1,21 @@
+__author__ = 'ralph'
+
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+
+    window.exitonclick()
+
+draw_square()
