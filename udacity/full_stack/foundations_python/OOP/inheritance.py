324808966b22fa430e6e9117989fda78b24dec8f
Ralph <beresfordjunior@me.com>
2015-04-06 11:32:20 +0100

    working inhertance example of using a class method overiding a parent class method

--- udacity/full_stack/foundations_python/OOP/inheritance.py
+++ udacity/full_stack/foundations_python/OOP/inheritance.py
@@ -18,8 +18,13 @@ class Child(Parent):
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
 
+    def show_info(self):
+        print("Last Name - "+self.last_name)
+        print("Eye Color - "+self.eye_color)
+        print("Number of toys - "+str(self.number_of_toys))
 
-# billy_cyrus = Parent("Cyrus", "blue")
+
+billy_cyrus = Parent("Cyrus", "blue")
 # billy_cyrus.show_info()
 
 miley_cyrus = Child("Cyrus", "Blue", 5)
