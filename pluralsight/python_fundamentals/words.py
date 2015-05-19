ce2feff997ba636db02c65a17ddfae95a9105327
Ralph <beresfordjunior@me.com>
2015-05-19 17:28:00 +0100

    Added documentation

--- pluralsight/python_fundamentals/words.py
+++ pluralsight/python_fundamentals/words.py
@@ -3,6 +3,15 @@ from urllib.request import urlopen
 
 
 def fetch_words(url):
+	"""Fetch a list of words from a URL.
+
+	Args:
+		url: The URL of a UTF-8 text document.
+
+	Returns:
+		A list of strings containing the words from
+		the document.
+	"""
 	with urlopen(url) as story:
 		story_words =[]
 		for line in story:
