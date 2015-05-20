b8387e84603d9a3d50d89823d17f8894e2e5ae86
Ralph <beresfordjunior@me.com>
2015-05-20 13:49:22 +0100

    Added print statment to print message to the standard error stream before returning

--- pluralsight/python_fundamentals/exceptional.py
+++ pluralsight/python_fundamentals/exceptional.py
@@ -1,9 +1,15 @@
 '''A module for demonstrating exceptiosn.'''
 
 
+import sys
+
+
 def convert(s):
 	'''Convert to an integer.'''
 	try:
 	 	return int(s)
-	except (ValueError, TypeError) as e:
+	except (ValueError, TypeError) as e :
+		print("Conversion error: {}"\
+				.format(str(e)),
+				file=sys.stderr)
 		return -1
