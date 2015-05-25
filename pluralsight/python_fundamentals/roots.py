760dc30d6b419f8f8e7055e8d2c1d34f180e6c93
Ralph <beresfordjunior@me.com>
2015-05-25 15:04:35 +0100

    handled zerodivision error

--- pluralsight/python_fundamentals/roots.py
+++ pluralsight/python_fundamentals/roots.py
@@ -19,6 +19,13 @@ def sqrt(x):
 	def main():
 		print(sqrt(9))
 		print(sqrt(2))
+		try:
+			print(sqrt(-1))
+		except ZeroDivisionError:
+			print("Cannot compute squrare root of a negative number.")
 
+		print("Program execution continues normally here.")
+
+		
 	if __name__ == '__main__':
 		main()
