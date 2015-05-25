9294e33defc68e606e43b9f7681180f82662d498
Ralph <beresfordjunior@me.com>
2015-05-25 15:06:21 +0100

    Refactored so there is a single try except block

--- pluralsight/python_fundamentals/roots.py
+++ pluralsight/python_fundamentals/roots.py
@@ -17,15 +17,16 @@ def sqrt(x):
 
 
 	def main():
-		print(sqrt(9))
-		print(sqrt(2))
 		try:
+			print(sqrt(9))
+			print(sqrt(2))
 			print(sqrt(-1))
+			print("This is never printed")
 		except ZeroDivisionError:
 			print("Cannot compute squrare root of a negative number.")
 
 		print("Program execution continues normally here.")
 
-		
+
 	if __name__ == '__main__':
 		main()
