8d81dc2ea5ef8b1193715f4433fc3bb7565888f7
Ralph <beresfordjunior@me.com>
2015-05-27 12:55:25 +0100

    initial iterator example

---
+++ pluralsight/python_fundamentals/iteration_protocols.py
@@ -0,0 +1,9 @@
+__author__ = 'ralph'
+
+iterable = ['Spring', 'Summer', 'Autumn', 'Winter']
+
+iterator = iter(iterable)
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
