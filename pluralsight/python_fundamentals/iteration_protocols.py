e6219c14f7b517101279b45c5b2e68ab1cb922ae
Ralph <beresfordjunior@me.com>
2015-05-27 13:00:25 +0100

    basic example of iterator protocols

--- pluralsight/python_fundamentals/iteration_protocols.py
+++ pluralsight/python_fundamentals/iteration_protocols.py
@@ -2,8 +2,19 @@ __author__ = 'ralph'
 
 iterable = ['Spring', 'Summer', 'Autumn', 'Winter']
 
-iterator = iter(iterable)
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+# iterator = iter(iterable)
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+
+
+# Here is a utility function
+def first(iterable):
+    iterator = iter(iterable)
+    try:
+        return next(iterator)
+    except StopIteration:
+        raise ValueError("iterable is empty")
+
+print(first(["sasd", "2nd", "safdsafafsdfas"]))
