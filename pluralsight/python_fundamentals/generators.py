5bccc054bb49459bafbd92c1eb5c01131a0e019e
Ralph <beresfordjunior@me.com>
2015-05-27 14:44:13 +0100

    another generator

--- pluralsight/python_fundamentals/generators.py
+++ pluralsight/python_fundamentals/generators.py
@@ -14,3 +14,14 @@ def gen123():
 
 for v in gen123():
     print(v)
+
+def gen246():
+    print("About to yield 2")
+    yield 2
+    print("About to yield 4")
+    yield 4
+    print("About to yield 6")
+    yield 6
+    print("About to return")
+
+g = gen246()
