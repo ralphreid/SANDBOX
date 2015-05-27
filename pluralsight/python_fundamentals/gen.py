54204e5954d443d21b737fdcf513e78287ffd801
Ralph <beresfordjunior@me.com>
2015-05-27 15:17:40 +0100

    added distinct generator

--- pluralsight/python_fundamentals/gen.py
+++ pluralsight/python_fundamentals/gen.py
@@ -25,5 +25,27 @@ def run_take():
     for item in take(3, items):
         print(item)
 
+def distinct(iterable):
+    """Return unique items by eliminating duplicates.
+
+    Args:
+        iterable: The source series.
+
+    Yields:
+        Unique elements in order from 'iterable'.
+    """
+
+    seen = set()
+    for item in iterable:
+        if item in seen:
+            continue
+        yield item
+        seen.add(item)
+
+def run_distinct():
+    items = [5, 7, 7, 6, 5, 5]
+    for item in distinct(items):
+        print(item)
+
 if __name__ == '__main__':
     run_take()
