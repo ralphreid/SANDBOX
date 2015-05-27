1c1a5490812143acc8bb883374bbf6c701006470
Ralph <beresfordjunior@me.com>
2015-05-27 10:26:19 +0100

    clean-up actions problems

--- pluralsight/python_fundamentals/clean_up_actions.py
+++ pluralsight/python_fundamentals/clean_up_actions.py
@@ -6,5 +6,7 @@ import os
 def make_at(path, dir_name):
     original_path = os.getcwd()
     os.chdir(path)
+    # But what happens if this fails then
     os.mkdir(dir_name)
+    # this wont't happen
     os.chdir(original_path)
