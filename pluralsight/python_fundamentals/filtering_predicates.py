5ef228c505c31c07bf5ba9d084bfca699436de0c
Ralph <beresfordjunior@me.com>
2015-05-27 12:25:38 +0100

    Added an initial predicate method

---
+++ pluralsight/python_fundamentals/filtering_predicates.py
@@ -0,0 +1,11 @@
+__author__ = 'ralph'
+
+from math import sqrt
+
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
