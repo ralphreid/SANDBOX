e44f959df5b948d6324590d30e4ce65355c72b07
Ralph <beresfordjunior@me.com>
2015-05-28 15:39:04 +0100

    Added Class Invariants to init function

--- pluralsight/python_fundamentals/classes/airtravel.py
+++ pluralsight/python_fundamentals/classes/airtravel.py
@@ -5,7 +5,17 @@ __author__ = 'ralph'
 class Flight:
 
     def __init__(self, number):
-        self._number = number
+        # String slicing used below
+        # Also, logical negation operator 'not' used
+        # Class Invariants are below
+        if not number[:2].isalpha():
+            raise ValueError("No airline code in '{}'".format(number))
+
+        if not number[:2].isupper():
+            raise ValueError("Invalid airline code in '{}'".format(number))
+
+        if not number[2:].isdigit() and init(number[2:]) <= 9999:
+            raise ValueError("Invalid route number '{}'".format(number))
 
     def number(self):
         return self._number
