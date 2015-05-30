b8a2a2d30ed08f75d26727e74d528408b2828359
Ralph <beresfordjunior@me.com>
2015-05-30 11:46:10 +0100

    Added base class

--- pluralsight/python_fundamentals/classes/airtravel.py
+++ pluralsight/python_fundamentals/classes/airtravel.py
@@ -152,6 +152,14 @@ class Flight:
                     # yields the passenter and seat as found
                     yield (passenger, "{}{}".format(row, letter))
 
+# Base class
+class Aircraft:
+
+    def num_seats(self):
+        rows, row_seats = self.seating_plan()
+        return len(rows) * len(row_seats)
+
+
 class AirbusA319:
     def __init__(self, registration):
         self._registration = registration
