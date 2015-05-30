168e80f856c8e23a334a3da7891fa1d70fdc3b0c
Ralph <beresfordjunior@me.com>
2015-05-30 11:28:27 +0100

    Removed Aircraft & use Duck Typing

--- pluralsight/python_fundamentals/classes/airtravel.py
+++ pluralsight/python_fundamentals/classes/airtravel.py
@@ -152,26 +152,18 @@ class Flight:
                     # yields the passenter and seat as found
                     yield (passenger, "{}{}".format(row, letter))
 
-class Aircraft:
-    # In production, we should validate as wells to that we can ensure
-    # for example that the number of seats is NOT negative
-    def __init__(self, registration, model, num_rows, num_seats_per_row):
+class AirbusA319:
+    def __init__(self, registration):
         self._registration = registration
-        self._model = model
-        self._num_rows = num_rows
-        self._num_seats_per_row = num_seats_per_row
 
     def registration(self):
         return self._registration
 
     def model(self):
-        return self._model
+        return "Airbus A319"
 
     def seating_plan(self):
-        # Added method which returns the allowed rows and seats as a tupl of range and seat letters
-        return (range(1, self._num_rows + 1), # Range function produces an iterable series of row numbers up to the number in the plane
-                "ABCDEFGHJK"[:self._num_seats_per_row]) # String and slice method return a string with one character per seat
-
+        return range(1, 23), "ABCDEF"
 
 # Convinience method so that I do not have to add flight and passengers every time
 def make_flight():
