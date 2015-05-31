bf87de55c4a3950c5db94bb44f01ff2c4885cee3
Ralph <beresfordjunior@me.com>
2015-05-31 12:54:51 +0100

    Added data sequencer

---
+++ pluralsight/python_fundamentals/files_and_resource_management/recaman.py
@@ -0,0 +1,33 @@
+__author__ = 'ralph'
+
+import sys
+from itertools import count, islice
+
+# contains a generator for yielding the recman numbers
+def sequence():
+    """Generate Recaman's sequence."""
+    seen = set()
+    a = 0
+    for n in count(1):
+        yield a
+        seen.add(a)
+        c = a - n
+        if c < 0 or c in seen:
+            c = a + n
+        a = c
+
+# Writes the start of the sequence to a file
+def write_sequence(filename, num):
+    """Write Recaman's sequence to a pext file."""
+    f = open(filename, mode='wt', encoding='utf-8')
+    # Generator function to covert the start of the sequence to a file
+    # Generator expression used to convert each number to a string and add
+    # a new line
+    f.writelines("{0}\n".format(r)
+                 for r in islice(sequence(), num + 1))  # islice used to truncate
+                # the infinate sequence
+    f.close()
+
+if __name__ == '__main__':
+    write_sequence(filename=sys.argv[1],
+                   num=int(sys.argv[2]))
