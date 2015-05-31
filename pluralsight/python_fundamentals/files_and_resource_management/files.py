e3f561d01f6273ea0c4fce741f224c1e09fb99a4
Ralph <beresfordjunior@me.com>
2015-05-31 12:30:19 +0100

    Initial example module to read and print text lines in a file

--- pluralsight/python_fundamentals/files_and_resource_management/files.py
+++ pluralsight/python_fundamentals/files_and_resource_management/files.py
@@ -1,1 +1,15 @@
 __author__ = 'ralph'
+
+
+import sys
+
+
+def main(filename):
+    f = open(filename, mode='rt', encoding='utf-8')
+    for line in f:
+        print(line)
+    f.close()
+
+if __name__ == '__main__':
+    # Gets the command line argument like python3 files.py wasteland.txt
+    main(sys.argv[1])
