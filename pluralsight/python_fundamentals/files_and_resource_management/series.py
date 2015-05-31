d1e563f07b6ddec01c7a5b9a1c07a6816d5a64e7
Ralph <beresfordjunior@me.com>
2015-05-31 13:09:39 +0100

    Refactored to use a list comprehension rather than for loop

--- pluralsight/python_fundamentals/files_and_resource_management/series.py
+++ pluralsight/python_fundamentals/files_and_resource_management/series.py
@@ -7,14 +7,10 @@ import sys
 def read_series(filename):
     try:
         f = open(filename, mode='rt', encoding='utf-8')
-        series = []
-        # uses a for-loop to iterate over the file reading one line at a time
-        for line in f:
-            a = int(line.strip())  # Strips the new line character & converts to interger
-            series.append(a)
+        # return a list comprehension
+        return [ int(line.strip()) for line in f ]
     finally:
         f.close()
-    return series
 
 def main(filename):
     series = read_series(filename)
