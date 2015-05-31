0fb2a19db8ac003849f96b9935fdcc4fa98141ed
Ralph <beresfordjunior@me.com>
2015-05-31 12:34:33 +0100

    Change print(line) to sys.stdout.write(line)

--- pluralsight/python_fundamentals/files_and_resource_management/files.py
+++ pluralsight/python_fundamentals/files_and_resource_management/files.py
@@ -7,7 +7,11 @@ import sys
 def main(filename):
     f = open(filename, mode='rt', encoding='utf-8')
     for line in f:
-        print(line)
+        # Since newline characters would print an extra line, lets use the standard out
+        # Write method of standard out stream is exactly the same write method
+        # used to write to the file
+        # Closely related and can be used because the stream is a file like object
+        sys.stdout.write(line)
     f.close()
 
 if __name__ == '__main__':
