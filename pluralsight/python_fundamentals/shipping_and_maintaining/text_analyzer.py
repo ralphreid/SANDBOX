44f27f77562af4bf26b308a3fe54ca4add877f4f
Ralph <beresfordjunior@me.com>
2015-06-01 11:40:36 +0100

    test fails because no functions have been defined yet

--- pluralsight/python_fundamentals/shipping_and_maintaining/text_analyzer.py
+++ pluralsight/python_fundamentals/shipping_and_maintaining/text_analyzer.py
@@ -5,8 +5,9 @@ import unittest
 class TextAnalysisTests(unittest.TestCase):
     """Tests for the ``analyze_text()`` function."""
 
-    def test_function_runs(self):
+    def test_function_runs(self):  # simple start with test_ as these are auto discovered
         """Basic smoke test: does the function run."""
+        # Will fail if it throws any exceptions
         analyze_text()
 
 if __name__ == '__main__':
