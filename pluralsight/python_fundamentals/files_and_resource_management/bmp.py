4552415977e042b814fc0b9f127d63e86fcddc0f
Ralph <beresfordjunior@me.com>
2015-06-01 11:05:10 +0100

    Typo

--- pluralsight/python_fundamentals/files_and_resource_management/bmp.py
+++ pluralsight/python_fundamentals/files_and_resource_management/bmp.py
@@ -77,34 +77,34 @@ def write_grayscale(filename, pixels):
         bmp.seek(pixel_offset_bookmark)
         bmp.write(_int32_to_bytes(pixel_data_bookmark))
 
-    def _bytes_to_int32(b):
-        """Convert a bytes object containing four bytes into an integer."""
-        return b[0] | (b[1] << 8) | (b[2] << 16) | (b[3] << 24)
+def _bytes_to_int32(b):
+    """Convert a bytes object containing four bytes into an integer."""
+    return b[0] | (b[1] << 8) | (b[2] << 16) | (b[3] << 24)
 
-    def dimensions(filename):
-        """Determine the dimensions in pixels of a BMP image.
+def dimensions(filename):
+    """Determine the dimensions in pixels of a BMP image.
 
-        Args:
-            filename: The filename of a BMP file.
+    Args:
+        filename: The filename of a BMP file.
 
-        Returns:
-            A tuple containing two integers with the width of height in pixels.
+    Returns:
+        A tuple containing two integers with the width of height in pixels.
 
-        Raises:
-            ValueError: If the file was not a BMP file.
-            OSError: If there was a problem reading the file.
-        """
-        with open(filename, 'rb') as f:
-            magic = f.read(2)
-            if magic != b'BM':  # Simple validation check by looking at the header
-                raise ValueError("{} is not a BMP file".format(filename))
+    Raises:
+        ValueError: If the file was not a BMP file.
+        OSError: If there was a problem reading the file.
+    """
+    with open(filename, 'rb') as f:
+        magic = f.read(2)
+        if magic != b'BM':  # Simple validation check by looking at the header
+            raise ValueError("{} is not a BMP file".format(filename))
 
-            f. seek(18)
-            width_bytes = f.read(4)
-            height_bytes = f.read(4)
+        f. seek(18)
+        width_bytes = f.read(4)
+        height_bytes = f.read(4)
 
-            return (_bytes_to_int32(width_bytes),
-                    _bytes_to_int32(height_bytes))
+        return (_bytes_to_int32(width_bytes),
+                _bytes_to_int32(height_bytes))
 
 
 def _int32_to_bytes(i):
