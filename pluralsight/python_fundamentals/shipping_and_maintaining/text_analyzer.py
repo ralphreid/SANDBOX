0152807999981ff016a175d498190e21962623a6
Ralph <beresfordjunior@me.com>
2015-06-01 11:45:09 +0100

    Added fixture

--- pluralsight/python_fundamentals/shipping_and_maintaining/text_analyzer.py
+++ pluralsight/python_fundamentals/shipping_and_maintaining/text_analyzer.py
@@ -9,6 +9,15 @@ def analyze_text():
 class TextAnalysisTests(unittest.TestCase):
     """Tests for the ``analyze_text()`` function."""
 
+    def setUp(self):
+        """Fixture that creates a file for the text methods to use."""
+        self.filename = 'text_analysis_test_file.txt'
+        with open(self.filename, 'w') as f:
+            f.write('Now we are engaged in a great ware.\n'
+                    'testing wheahter that nation,\n'
+                    'or any nation so concieved and so dedicated,\n'
+                    'can long endure.')
+
     def test_function_runs(self):  # simple start with test_ as these are auto discovered
         """Basic smoke test: does the function run."""
         # Will fail if it throws any exceptions
