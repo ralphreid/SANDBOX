96964901b157cffb991b4efccc06e6699a821738
Ralph <beresfordjunior@me.com>
2015-06-01 11:37:31 +0100

    Initial unittest skeleton

---
+++ pluralsight/python_fundamentals/shipping_and_maintaining/text_analyzer.py
@@ -0,0 +1,13 @@
+__author__ = 'ralph'
+
+import unittest
+
+class TextAnalysisTests(unittest.TestCase):
+    """Tests for the ``analyze_text()`` function."""
+
+    def test_function_runs(self):
+        """Basic smoke test: does the function run."""
+        analyze_text()
+
+if __name__ == '__main__':
+    unittest.main()
