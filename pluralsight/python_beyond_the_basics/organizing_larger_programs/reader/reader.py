1a89d79fe5bf122e2c32e8854400a1053080a3e0
Ralph <beresfordjunior@me.com>
2015-06-02 12:40:31 +0100

    Added the ability for reader to read multiple compression types

--- pluralsight/python_beyond_the_basics/organizing_larger_programs/reader/reader.py
+++ pluralsight/python_beyond_the_basics/organizing_larger_programs/reader/reader.py
@@ -1,28 +1,23 @@
 __author__ = 'ralph'
 
+import os
+
+from reader.compressed import bzipped, gzipped
+
+extension_map = {
+    '.bz2': bzipped.opener,
+    '.gz': gzipped.opener,
+}
+
 class Reader:
     def __init__(self, filename):
-        self.filename = filename
-        self.f = open(self.filename, 'rt')
+        extension = os.path.splitext(filename)[1]  # Checks the extension of the file
+        opener = extension_map.get(extension, open)  # If extension is in the map,
+        # special opener will be used, otherwise standard 'open' will be used
+        self.f = opener(filename, 'rt')
 
     def close(self):
         self.f.close()
 
     def read(self):
         return self.f.read()
-
-
-
-# Examples at the REPL:
-# import reader.reader
-# reader.reader.__file__
-# r = reader.reader.Reader('reader/reader.py')
-# r.read()
-# r.close()
-
-# Examples at the REPL - After Hoisting:
-# import reader
-# reader.__file__
-# r = reader.Reader('reader/reader.py')
-# r.read()
-# r.close()
