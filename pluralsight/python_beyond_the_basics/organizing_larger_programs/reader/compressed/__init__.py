131d5bbf93a6f5cf0a9c0d3cecba86fb83ee1c29
Ralph <beresfordjunior@me.com>
2015-06-02 13:52:21 +0100

    Added __all__ configs

--- pluralsight/python_beyond_the_basics/organizing_larger_programs/reader/compressed/__init__.py
+++ pluralsight/python_beyond_the_basics/organizing_larger_programs/reader/compressed/__init__.py
@@ -1,1 +1,6 @@
 __author__ = 'ralph'
+
+from reader.compressed.bzipped import opener as bz2_opener
+from reader.compressed.gzipped import opener as gzip_opener
+
+__all__ = ['bz2_opener', 'gzip_opener']
