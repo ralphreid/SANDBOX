db6fffe52f4a3b6d874691d0ff473150a19eca38
Ralph <beresfordjunior@me.com>
2015-06-22 17:21:54 +0100

    Added Game class

--- pluralsight/django_fundamentals/boardgame/tictactoe/models.py
+++ pluralsight/django_fundamentals/boardgame/tictactoe/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    first_player = models.ForeignKey(User, related_name="games_first_player")
+    second_player = models.ForeignKey(User,related_name="games_second_player")
+    next_to_move = models.ForeignKey(User, related_name="games_to_move")
+    start_time = models.DateTimeField(auto_now_add=True)
+    last_active = models.DateTimeField(auto_now=True)
+
+
 class Move(models.Model):
     x = models.IntegerField()
     y = models.IntegerField()
