120df80eec7f5fa20a4fd5b034b9367cd276fafb
Ralph <beresfordjunior@me.com>
2015-06-22 17:01:40 +0100

    Updated Move model

--- pluralsight/django_fundamentals/boardgame/tictactoe/models.py
+++ pluralsight/django_fundamentals/boardgame/tictactoe/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Move(models.Model)
+class Move(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    comment = models.CharField(max_length=300)
