9c5d8060059ff7322666c12e9d68988537d200d2
Ralph <beresfordjunior@me.com>
2015-06-23 16:04:13 +0100

    Added urlpatterns

--- pluralsight/django_fundamentals/boardgame/boardgame/urls.py
+++ pluralsight/django_fundamentals/boardgame/boardgame/urls.py
@@ -24,3 +24,16 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'main.views.home')
 )
+
+
+urlpatterns += patterns(
+    'django.contrib.auth.views',
+
+    url(r'^login/$', 'login',
+        {'template_name': 'login.html'},
+        name='boardgames_login'),
+
+    url(r'^logout/$', 'logout',
+        {'next_page': 'boardgames_home'},
+        name='boardgames_logout'),
+)
