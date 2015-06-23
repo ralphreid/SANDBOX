4d85fff8b16303b33de758a7676950348583940e
Ralph <beresfordjunior@me.com>
2015-06-23 11:25:03 +0100

    registered classes

--- pluralsight/django_fundamentals/boardgame/main/admin.py
+++ pluralsight/django_fundamentals/boardgame/main/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 
-from .models import Game, Move
-
-
-admin.site.register(Game)
-admin.site.register(Move)
-
+# Register your models here.
--- pluralsight/django_fundamentals/boardgame/tictactoe/admin.py
+++ pluralsight/django_fundamentals/boardgame/tictactoe/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, Move
+
+
+admin.site.register(Game)  #Register with admin interface so they will show up in the user interface
+admin.site.register(Move)
