ce9153263cbb67d1b154dda2056483a056229b46
Ralph <beresfordjunior@me.com>
2015-06-23 11:39:48 +0100

    Added game status choices

--- pluralsight/django_fundamentals/boardgame/tictactoe/models.py
+++ pluralsight/django_fundamentals/boardgame/tictactoe/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User  # Taking advantage of a Built-In user class
 
 
+# All possible status options for a game
+GAME_STATUS_CHOICES = (
+    ('A', 'Active'),
+    ('F', 'First Player Wins'),
+    ('S', 'Second Player Wins'),
+    ('D', 'Draw'),
+)
+
 class Game(models.Model):
     # Remember all django models must inherit from the Django base model class 'models'
     first_player = models.ForeignKey(User, related_name="games_first_player")
@@ -9,8 +17,8 @@ class Game(models.Model):
     next_to_move = models.ForeignKey(User, related_name="games_to_move")
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, default='A', # Parameter showing the game status, default is 'A' for active
-                              choices=GAME_STATUS_CHOICES) # Makes the possible options explist using the choices value referencing list of choices
+    status = models.CharField(max_length=1, default='A',  # Parameter showing the game status, default is 'A' for active
+                              choices=GAME_STATUS_CHOICES)  # Makes the possible options explist using the choices value referencing list of choices
 
 
     def __str__(self): # method to allow a Game object to display itself
