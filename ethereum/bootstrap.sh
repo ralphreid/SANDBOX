87e95521c4cf325ed36b6612566fa87ded654784
Ralph <beresfordjunior@me.com>
2015-08-27 14:19:40 +0100

    added bootstrap

--- ethereum/bootstrap.sh
+++ ethereum/bootstrap.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# from https://github.com/ethereum/go-ethereum/wiki/Installation-Instructions-for-Ubuntu
+sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo add-apt-repository -y ppa:ethereum/ethereum-dev
+
+# Update apt surpress output and auto yes
+apt-get -qqy update
+
+sudo apt-get install ethereum
