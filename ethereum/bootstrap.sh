024b3cb15bcec582a35e91bf8451b48e0f3fa35e
Ralph <beresfordjunior@me.com>
2015-08-27 14:35:55 +0100

    updated

--- ethereum/bootstrap.sh
+++ ethereum/bootstrap.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
 # from https://github.com/ethereum/go-ethereum/wiki/Installation-Instructions-for-Ubuntu
-sudo apt-get install software-properties-common
-sudo add-apt-repository -y ppa:ethereum/ethereum
-sudo add-apt-repository -y ppa:ethereum/ethereum-dev
+# sudo apt-get install software-properties-common
+# sudo add-apt-repository -y ppa:ethereum/ethereum
+# sudo add-apt-repository -y ppa:ethereum/ethereum-dev
 
 # Update apt surpress output and auto yes
-apt-get -qqy update
+#sudo apt-get -y update
 
-sudo apt-get install ethereum
+#sudo apt-get install ethereum
+
+# from https://github.com/ethereum/cpp-ethereum/wiki/Building-on-Ubuntu
+# sudo apt-add-repository ppa:george-edison55/cmake-3.x
+
+# sudo apt-get -y update
+# sudo apt-get -y install language-pack-en-base
+# sudo dpkg-reconfigure locales
+# sudo apt-get -y install software-properties-common
+# wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
+# sudo add-apt-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty main"
+# sudo add-apt-repository -y ppa:ethereum/ethereum-qt
+# sudo add-apt-repository -y ppa:ethereum/ethereum
+# sudo add-apt-repository -y ppa:ethereum/ethereum-dev
+# sudo apt-get -y update
+# sudo apt-get -y upgrade
