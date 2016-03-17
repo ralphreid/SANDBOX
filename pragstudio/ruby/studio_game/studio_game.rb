559c4fe82332f584a01a89132ebc55b3270a957b
Ralph Reid <beresfordjunior@me.com>
2016-03-16 19:30:13 -0700

    03-numbers-strings

--- pragstudio/ruby/studio_game/studio_game.rb
+++ pragstudio/ruby/studio_game/studio_game.rb
@@ -1,10 +1,12 @@
-greatings = "Welcome!"
+name1 = 'larry'
 
-3.times do 
-  puts greatings.upcase
-end
-
-puts Time.new
+health1 = 6
 
 
+puts "#{name1}'s health is #{health1 / 9.0}"
 
+name1 = 'larry'
+name2 = 'curly'
+name3 = 'moe'
+
+puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
