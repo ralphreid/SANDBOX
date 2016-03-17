4a163cbf5b89bd6c303c79955b80973ae71e0724
Ralph Reid <beresfordjunior@me.com>
2016-03-16 19:34:07 -0700

    03-numbers-strings

---
+++ pragstudio/ruby/crowdfund/crowdfund.rb
@@ -0,0 +1,8 @@
+project1 = 'Project ABC'
+project2 = 'Project LMN'
+project3 = 'Project XYZ'
+
+funding1 = 1000
+
+puts "#{project1} has $#{funding1} in funding."
+puts "Projects: \n\t#{project1}\n\t#{project2}\n\t#{project3}"
