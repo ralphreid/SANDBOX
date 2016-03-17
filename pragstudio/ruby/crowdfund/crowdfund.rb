001fcc959c18c59fab2aeec6f7a4e685a5f83e42
Ralph Reid <beresfordjunior@me.com>
2016-03-17 12:48:12 -0700

    04-variables-objects

--- pragstudio/ruby/crowdfund/crowdfund.rb
+++ pragstudio/ruby/crowdfund/crowdfund.rb
@@ -1,8 +1,17 @@
 project1 = 'Project ABC'
 project2 = 'Project LMN'
 project3 = 'Project XYZ'
-
 funding1 = 1000
+funding2 = 500
+funding3 = 25
 
 puts "#{project1} has $#{funding1} in funding."
+puts "#{project1.capitalize} has $#{funding1} in funding."
+puts "#{project2.upcase} has $#{funding2} in funding."
+puts "#{project3.ljust(30, '.')} $#{funding3} in funding."
+
 puts "Projects: \n\t#{project1}\n\t#{project2}\n\t#{project3}"
+
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+puts "Funding amounts as of #{formatted_time}."
--- pragstudio/ruby/studio_game/studio_game.rb
+++ pragstudio/ruby/studio_game/studio_game.rb
@@ -1,12 +1,32 @@
 name1 = 'larry'
+health1 = 60
+puts "#{name1.capitalize} has a health of #{health1}."
 
-health1 = 6
-
-
-puts "#{name1}'s health is #{health1 / 9.0}"
-
-name1 = 'larry'
 name2 = 'curly'
-name3 = 'moe'
+health2 = 125
+puts "#{name2.upcase} has a health of #{health2}."
 
+name3 = "moe"
+health3 = 100
+
+text = "#{name3.capitalize} has a health of #{health3}."
+puts text.center(50, '*')
+
+# or chain it together on one line:
+
+puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
+
+name4 = 'shemp'
+health4 = 90
+
+formatted_name = name4.capitalize.ljust(30, '.')
+puts "#{formatted_name} #{health4} health"
+
+# or you chain it together on one line:
+
+puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
 puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
+
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+puts "The game started on #{formatted_time}"
