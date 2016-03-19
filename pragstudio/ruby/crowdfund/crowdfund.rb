e06faf9b7671c2dd9bdfebe092eb9d39b533f4c9
Ralph Reid <beresfordjunior@me.com>
2016-03-19 12:04:27 -0700

    classes

--- pragstudio/ruby/crowdfund/crowdfund.rb
+++ pragstudio/ruby/crowdfund/crowdfund.rb
@@ -1,14 +1,46 @@
-def time
-	  current_time = Time.new
-	    current_time.strftime("%A, %B %d, %Y")
+class Project
+
+  def initialize(name, target_funding_amount, funding=0)
+    @name = name
+    @target = target_funding_amount
+    @funding = funding
+  end
+  
+  def to_s
+    "#{@name} has $#{@funding} in funding towards a goal of $#{@target}." 
+  end
+
+  def remove_funds
+    @funding -= 15
+    puts "#{@name} lost some funds!"
+  end
+  
+  def add_funds
+    @funding += 25
+    puts "#{@name} got more funds!"
+  end
 end
 
-def project_listing(name, funding=0)
-	  "#{name} has $#{funding} in funding as of #{time}."
-end
+project1 = Project.new("Project ABC", 5000, 1000)
+puts project1
 
-puts project_listing("Project ABC", 1000)
-puts project_listing("Project LMN", 500)
-puts project_listing("Project XYZ", 25)
-puts project_listing("Project TBD")
+project2 = Project.new("Project LMN", 3000, 500)
+puts project2
 
+project3 = Project.new("Project XYZ", 75, 25)
+puts project3
+
+project4 = Project.new("Project TBD", 10000)
+puts project4
+
+puts "***"
+project1.remove_funds
+project2.remove_funds
+project3.add_funds
+project4.add_funds
+puts "***"
+
+puts project1
+puts project2
+puts project3
+puts project4
--- pragstudio/ruby/studio_game/studio_game.rb
+++ pragstudio/ruby/studio_game/studio_game.rb
@@ -1,12 +1,33 @@
-def time
-  Time.new.strftime("%I:%M:%S")
+class Player
+  
+  def initialize (name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def to_s
+    "I'm #{@name} with a health of #{@health}." 	
+  end
+
+  def blam
+    @health -= 10  	
+    puts "#{@name} got blammed!"
+  end
+
+  def w00t
+  	@health += 15
+    puts "#{@name} got w00ted!"
+  end
 end
 
-def say_hello (name, health=100)
-  "I'm #{name.capitalize} with a health of #{health} as of #{time}"
-end
+player1 = Player.new("moe")
+puts player1
 
-puts say_hello("larry", 60)
-puts say_hello("curly", 125)
-puts say_hello("moe")
-puts say_hello("shemp", 90)
+player2 = Player.new("larry",60)
+puts player2
+
+player3 = Player.new("curly",125)
+puts player3
+player3.blam
+player3.w00t
+puts player3
