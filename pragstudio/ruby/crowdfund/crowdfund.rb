95a38165a2f46ecfbf644c05ed2684050033ca0a
Ralph Reid <beresfordjunior@me.com>
2016-03-18 19:01:04 -0700

    methods

--- pragstudio/ruby/crowdfund/crowdfund.rb
+++ pragstudio/ruby/crowdfund/crowdfund.rb
@@ -1,17 +1,14 @@
-project1 = 'Project ABC'
-project2 = 'Project LMN'
-project3 = 'Project XYZ'
-funding1 = 1000
-funding2 = 500
-funding3 = 25
+def time
+	  current_time = Time.new
+	    current_time.strftime("%A, %B %d, %Y")
+end
 
-puts "#{project1} has $#{funding1} in funding."
-puts "#{project1.capitalize} has $#{funding1} in funding."
-puts "#{project2.upcase} has $#{funding2} in funding."
-puts "#{project3.ljust(30, '.')} $#{funding3} in funding."
+def project_listing(name, funding=0)
+	  "#{name} has $#{funding} in funding as of #{time}."
+end
 
-puts "Projects: \n\t#{project1}\n\t#{project2}\n\t#{project3}"
+puts project_listing("Project ABC", 1000)
+puts project_listing("Project LMN", 500)
+puts project_listing("Project XYZ", 25)
+puts project_listing("Project TBD")
 
-current_time = Time.new
-formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
-puts "Funding amounts as of #{formatted_time}."
--- pragstudio/ruby/studio_game/studio_game.rb
+++ pragstudio/ruby/studio_game/studio_game.rb
@@ -1,32 +1,12 @@
-name1 = 'larry'
-health1 = 60
-puts "#{name1.capitalize} has a health of #{health1}."
+def time
+  Time.new.strftime("%I:%M:%S")
+end
 
-name2 = 'curly'
-health2 = 125
-puts "#{name2.upcase} has a health of #{health2}."
+def say_hello (name, health=100)
+  "I'm #{name.capitalize} with a health of #{health} as of #{time}"
+end
 
-name3 = "moe"
-health3 = 100
-
-text = "#{name3.capitalize} has a health of #{health3}."
-puts text.center(50, '*')
-
-# or chain it together on one line:
-
-puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
-
-name4 = 'shemp'
-health4 = 90
-
-formatted_name = name4.capitalize.ljust(30, '.')
-puts "#{formatted_name} #{health4} health"
-
-# or you chain it together on one line:
-
-puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
-puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
-
-current_time = Time.new
-formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
-puts "The game started on #{formatted_time}"
+puts say_hello("larry", 60)
+puts say_hello("curly", 125)
+puts say_hello("moe")
+puts say_hello("shemp", 90)
