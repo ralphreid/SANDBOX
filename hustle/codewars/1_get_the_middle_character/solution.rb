7ecf6c6746eca9652b1af7a2f31d800087925945
Ralph Reid <ralph.reid@dreamworks.com>
2017-10-18 13:17:23 -0700

    Initial quiestion

---
+++ hustle/codewars/1_get_the_middle_character/solution.rb
@@ -0,0 +1,3 @@
+def get_middle(s)
+  #your code here
+end
---
+++ hustle/codewars/1_get_the_middle_character/test.rb
@@ -0,0 +1,7 @@
+Test.describe("Basic tests") do
+  Test.assert_equals(get_middle("test"),"es")
+  Test.assert_equals(get_middle("testing"),"t")
+  Test.assert_equals(get_middle("middle"),"dd")
+  Test.assert_equals(get_middle("A"),"A")
+  Test.assert_equals(get_middle("of"),"of")
+end
