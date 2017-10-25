e0c0249b80f2b3a93c2c351ec9854f5f96f144ca
Ralph Reid <ralph.reid@dreamworks.com>
2017-10-24 20:18:19 -0700

    completed kata

--- hustle/codewars/1_get_the_middle_character/lib/solution.rb
+++ hustle/codewars/1_get_the_middle_character/lib/solution.rb
@@ -1,3 +1,16 @@
 def get_middle(s)
-  #your code here
+  length = s.length.to_i
+  case
+    when length == 1, length == 2
+      return s
+    when length >= 1 && length.odd?
+      index = length / 2
+      return s[index]
+    when length >= 2 && length.even?
+      index_left = length / 2 - 1
+      index_right = length / 2
+      return s[index_left] + s[index_right]
+    else
+      return 'else'
+  end
 end
