5ffef4030e2c5f9a734109846f9abea60d1ab923
Ralph Reid <beresfordjunior@me.com>
2018-04-11 08:48:52 -0700

    still adjusting

--- terraform/linux_virtual_machine_infrastructure/terraform_azure.tf
+++ terraform/linux_virtual_machine_infrastructure/terraform_azure.tf
@@ -5,10 +5,10 @@ variable "resourcename" {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
     version = "~> 1.3"
-    subscription_id = "${ARM_SUBSCRIPTION_ID}"
-    client_id       = "${ARM_CLIENT_ID}"
-    client_secret   = "${ARM_CLIENT_SECRET}"
-    tenant_id       = "${ARM_TENANT_ID}"
+    subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
+    client_id       = "${var.ARM_CLIENT_ID}"
+    client_secret   = "${var.ARM_CLIENT_SECRET}"
+    tenant_id       = "${var.ARM_TENANT_ID}"
 }
 
 # Create a resource group if it doesn’t exist
