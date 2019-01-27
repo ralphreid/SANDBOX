98a8c3f14d5409a9e85b950734a90c96d104d3fd
Ralph Reid <beresfordjunior@me.com>
2019-01-27 15:16:42 -0800

    Update solution_1.py

--- leetcode/7_Reverse_Integer/solution_1.py
+++ leetcode/7_Reverse_Integer/solution_1.py
@@ -4,11 +4,5 @@ class Solution:
         :type x: int
         :rtype: int
         """
-        flag=1 if x>0 else -1
-        x=x if x>0 else -x
-        res=0
-        while x>0:
-            if res*10.0 + x%10 > 2147483647:return 0
-            res = res*10+x%10
-            x/=10
-        return res*flag   
+        if x < 0:   return int(str(x)[1:][::-1])*-1
+        else:       return int(str(x)[::-1])
