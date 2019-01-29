ff7e4f86b2f7f5a2b6ad064777179413ad7ea314
Ralph Reid <beresfordjunior@me.com>
2019-01-28 17:32:27 -0800

    Completed function

--- exercism/pangram/pangram.py
+++ exercism/pangram/pangram.py
@@ -1,3 +1,13 @@
+from string import ascii_lowercase
+from string import punctuation
+
+
 def is_pangram(sentence):
     if not sentence:
         return False
+    else:
+        alphabet = set(ascii_lowercase)
+        sentence = sentence.lower()
+        tr = str.maketrans("", "", punctuation)
+        sentence = sentence.translate(tr)
+        return set(sentence) >= alphabet
