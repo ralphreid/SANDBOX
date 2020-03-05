27180ee1925f63aef56e6d3b64d10bd9e454cdc6
Ralph Reid <ralph.reid@distilled.net>
2020-03-05 13:54:58 +0000

    Added a command

---
+++ acloud/aws-ecs-scaling-docker/sandbox_prod_cluster.sh
@@ -0,0 +1,1 @@
+ecs-cli up --capability-iam --security-group ralph_SG_euwest1 --vpc vpc-a4aab4c2 --subnets subnet-20cb5f7a,subnet-6e9dd608,subnet-8a2b71c2 --launch-type FARGATE --cluster production --debug
