c7455bcea507ebe907a95c928996c94f06c3c951
Ralph Reid <ralph.reid@distilled.net>
2020-03-11 14:47:19 +0000

    Added security groups

--- acloud/aws-ecs-scaling-docker/production/_tips_tricks/sandbox_prod_cluster.sh
+++ acloud/aws-ecs-scaling-docker/production/_tips_tricks/sandbox_prod_cluster.sh
@@ -5,3 +5,7 @@ aws elbv2 create-load-balancer --name dockerzon-web --security-groups sg-05db872
 aws elbv2 create-target-group --name targets-dockerzon --protocol HTTP --port 80 --target-type ip --health-check-protocol HTTP --health-check-port 80 --health-check-path "/health_check" --health-check-timeout-seconds 5 --health-check-interval-seconds 30 --healthy-threshold-count 10 --unhealthy-threshold-count 2 --vpc-id vpc-a4aab4c2
 aws elbv2 create-listener --load-balancer-arn arn:aws:elasticloadbalancing:eu-west-1:481461972950:loadbalancer/app/dockerzon-web/00ba132830264b5a --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=arn:aws:elasticloadbalancing:eu-west-1:481461972950:targetgroup/targets-dockerzon/4caf4db04bb50aa0
 aws elbv2 modify-load-balancer-attributes --load-balancer-arn arn:aws:elasticloadbalancing:eu-west-1:481461972950:loadbalancer/app/dockerzon-web/00ba132830264b5a --attributes Key=idle_timeout.timeout_seconds,Value=5
+
+
+
+# Create CF Dependencies
